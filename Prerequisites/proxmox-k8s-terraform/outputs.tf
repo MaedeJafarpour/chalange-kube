850ca72520d2ecfe99d52e92081eb8f912f44b26
maedejafarpour <jp.maede83@gmail.com>
2025-04-21 03:14:54 +0330

    initial commit

--- Prerequisites/proxmox-k8s-terraform/outputs.tf
+++ Prerequisites/proxmox-k8s-terraform/outputs.tf
@@ -1,15 +1,35 @@
-output "master_ips" {
+output "cluster_endpoints" {
+  description = "Kubernetes cluster access endpoints"
   value = {
-    for vm in proxmox_vm_qemu.k8s_master :
-    vm.name => vm.default_ipv4_address
+    load_balancer_ip = proxmox_vm_qemu.load_balancer.default_ipv4_address
+    masters          = module.k8s_masters.master_ips
+    workers          = module.k8s_workers.worker_ips
+  }
+  sensitive = true
+}
+
+output "ha_status" {
+  description = "Proxmox HA status"
+  value = var.ha_config.enabled ? {
+    ha_group  = var.ha_config.ha_group
+    members   = module.k8s_masters.master_names
+    policy    = var.ha_config.migration_policy
+  } : null
+}
+
+output "monitoring_endpoints" {
+  description = "Monitoring stack access URLs"
+  value = {
+    prometheus = "http://${proxmox_vm_qemu.load_balancer.default_ipv4_address}:9090"
+    grafana    = "http://${proxmox_vm_qemu.load_balancer.default_ipv4_address}:3000"
+    alertmanager = "http://${proxmox_vm_qemu.load_balancer.default_ipv4_address}:9093"
   }
 }
 
-output "worker_ips" {
+output "backup_schedule" {
+  description = "Cluster backup schedule"
   value = {
-    for vm in proxmox_vm_qemu.k8s_worker :
-    vm.name => vm.default_ipv4_address
+    masters = var.masters.backup_policy
+    workers = var.workers.backup_policy
   }
 }
-
-
--- Prerequisites/proxmox-k8s-terraform/variables.tf
+++ Prerequisites/proxmox-k8s-terraform/variables.tf
@@ -1,109 +1,160 @@
+# Proxmox Connection Configuration
 variable "pm_api_url" {
-  description = "Proxmox API URL"
   type        = string
-}
-
-variable "pm_api_token_id" {
-  description = "Proxmox API token ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "pm_api_token_secret" {
-  description = "Proxmox API token secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "pm_node_name" {
-  description = "Proxmox node name"
-  type        = string
-  default     = "pve"
-}
-
-variable "master_nodes" {
-  description = "Number of master nodes"
-  type        = number
-  default     = 3
-}
-
-variable "worker_nodes" {
-  description = "Number of worker nodes"
-  type        = number
-  default     = 2
-}
-
-variable "vm_template" {
-  description = "VM template to clone"
-  type        = string
-  default     = "ubuntu-2204-cloudinit-template"
-}
-
-variable "vm_network" {
-  description = "Network bridge to use"
-  type        = string
-  default     = "vmbr0"
-}
-
-
-variable "ssh_key" {
-  description = "SSH public key"
-  type        = string
-}
-variable "pm_user" {
-  description = "Proxmox API user"
-  type        = string
-}
-variable "proxmox_node" {
-  description = "Name of the Proxmox node to create VMs on"
-  type        = string
-  default     = "pve"  # Change to your Proxmox node name
-}
-
-variable "template_name" {
-  description = "Name of the VM template to clone"
-  type        = string
-  default     = "ubuntu-2204-cloudinit"  # Change to your template
-}
-
-variable "master_count" {
-  description = "Number of master nodes"
-  type        = number
-  default     = 3
-}
-
-variable "worker_count" {
-  description = "Number of worker nodes"
-  type        = number
-  default     = 2
-}
-
-variable "master_vm_settings" {
-  type = object({
-    cores   = number
-    memory  = number
-    storage = string
-    disk_size = string
-  })
-  default = {
-    cores   = 2
-    memory  = 4096
-    storage = "local-lvm"
-    disk_size = "20G"
+  description = "Proxmox API endpoint URL (format: https://host:port/)"
+  validation {
+    condition     = can(regex("^https://.*:\\d+/?$", var.pm_api_url))
+    error_message = "Must be a valid HTTPS URL with port number"
   }
 }
 
-variable "worker_vm_settings" {
+variable "pm_user" {
+  type        = string
+  description = "Proxmox authentication user (format: user@realm)"
+  validation {
+    condition     = can(regex(".+@.+", var.pm_user))
+    error_message = "Must be in user@realm format"
+  }
+}
+
+variable "pm_api_token_id" {
+  type        = string
+  sensitive   = true
+  description = "Proxmox API token ID (format: user@realm!token-name)"
+  validation {
+    condition     = can(regex(".+@.+!.+", var.pm_api_token_id))
+    error_message = "Must be in user@realm!token-name format"
+  }
+}
+
+variable "pm_api_token_secret" {
+  type        = string
+  sensitive   = true
+  description = "Proxmox API token secret (UUID format)"
+  validation {
+    condition     = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.pm_api_token_secret))
+    error_message = "Must be a valid UUID format"
+  }
+}
+
+# Proxmox Infrastructure Configuration
+variable "proxmox_node" {
+  type        = string
+  default     = "pve"
+  description = "Target Proxmox cluster node name"
+}
+
+variable "storage_pool" {
+  type        = string
+  default     = "local-lvm"
+  description = "Default storage pool for VM disks"
+}
+
+variable "vm_template" {
+  type        = string
+  default     = "ubuntu-2204-cloudinit-template"
+  description = "Source VM template name for cloning"
+}
+
+# Network Configuration
+variable "network_config" {
   type = object({
-    cores   = number
-    memory  = number
-    storage = string
-    disk_size = string
+    bridge      = string
+    cidr        = string
+    gateway     = string
+    dns_servers = list(string)
+    vlan_tag    = optional(number)
   })
+  description = "Network configuration for cluster nodes"
   default = {
-    cores   = 4
-    memory  = 8192
-    storage = "local-lvm"
-    disk_size = "30G"
+    bridge      = "vmbr0"
+    cidr        = "192.168.1.0/24"
+    gateway     = "192.168.1.1"
+    dns_servers = ["192.168.1.1"]
+  }
+  validation {
+    condition     = can(cidrhost(var.network_config.cidr, 0))
+    error_message = "Must be a valid CIDR notation"
+  }
+}
+
+# SSH Configuration
+variable "ssh_config" {
+  type = object({
+    key_path     = string
+    agent        = bool
+    username     = string
+    sudo_password = optional(string, "")
+  })
+  description = "SSH configuration for node access"
+  default = {
+    key_path = "~/.ssh/id_rsa.pub"
+    agent    = true
+    username = "ubuntu"
+  }
+  sensitive = true
+}
+
+# High Availability Configuration
+variable "ha_config" {
+  type = object({
+    enabled          = bool
+    ha_group         = string
+    migration_policy = string
+    backup_schedule  = optional(string)
+  })
+  description = "Proxmox HA configuration"
+  default = {
+    enabled          = true
+    ha_group         = "k8s-cluster"
+    migration_policy = "manual"
+  }
+}
+
+# Master Node Configuration
+variable "masters" {
+  type = object({
+    count        = number
+    cores        = number
+    memory       = number
+    disk_size    = string
+    ip_start     = number
+    cpu_type     = optional(string, "host")
+    ballooning   = optional(bool, false)
+    backup_policy = optional(string, "daily")
+  })
+  description = "Configuration for Kubernetes master nodes"
+  default = {
+    count     = 3
+    cores     = 2
+    memory    = 8192
+    disk_size = "50G"
+    ip_start  = 100
+  }
+  validation {
+    condition     = var.masters.count >= 1 && var.masters.count <= 5
+    error_message = "Master count must be between 1-5 for HA"
+  }
+}
+
+# Worker Node Configuration
+variable "workers" {
+  type = object({
+    count          = number
+    cores          = number
+    memory         = number
+    disk_size      = string
+    ip_start       = number
+    cpu_type       = optional(string, "host")
+    ballooning     = optional(bool, true)
+    scaling_policy = optional(string, "balanced")
+  })
+  description = "Configuration for Kubernetes worker nodes"
+  default = {
+    count     = 2
+    cores     = 4
+    memory    = 16384
+    disk_size = "80G"
+    ip_start  = 110
   }
 }
